5c36acc15e64b4e27e1a775b56d7760ef8bd43a3
Даниил <linkjkeee@gmail.com>
2018-06-09 11:55:42 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+// Webpack 4: Webpack-starter config:
+
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin('dist', {}),
+    new ExtractTextPlugin({
+      filename: 'style.[hash].css'
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ],
+
+  devServer: {
+    // Display only errors to reduce the amount of output.
+    stats: "errors-only",
+    host: process.env.HOST, // Defaults to `localhost`
+    port: process.env.PORT, // Defaults to 8080
+    open: true, // Open the page in browser
+    overlay: true
+  },
+}
